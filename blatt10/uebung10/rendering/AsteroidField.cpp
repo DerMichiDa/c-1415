0d0ba70f9147eacc6d8b700baecec935228b650f
FrankosLosTankos <frank.mehne@gmx.de>
2015-01-04 20:37:12 +0100

    Update AsteroidField.cpp

--- blatt10/uebung10/rendering/AsteroidField.cpp
+++ blatt10/uebung10/rendering/AsteroidField.cpp
@@ -1,45 +1,48 @@
 #include "AsteroidField.hpp"
 #include "math/Randomizer.hpp"
+#include "List.tpp"
 
 namespace asteroids
 {
-
 
 AsteroidField::AsteroidField(int quantity, string basePath) :
     m_basePath(basePath)
 {
 	float scale;
 	Vertex<float> vert;
-	Asteroid asteroid;
 
-	m_asteroids = new List<Asteroid>();
- 	// Generate asteroids
+	// create List of Asteroids
+	m_asteroids = new List<Asteroid*>();
+
+ 	// generate asteroids
 	for(int i = 0; i < quantity; i++)
 	{
-	    TriangleMesh* mesh = TriangleMeshFactory::instance().getMesh(m_basePath + "asteroid.3ds");
+		// create asteroid mesh
+	    	TriangleMesh* mesh = TriangleMeshFactory::instance().getMesh(m_basePath + "asteroid.3ds");
 
-        vert = Randomizer::instance()->getRandomVertex(500);
-        scale = Randomizer::instance()->getRandomNumber(1, 5);
-        asteroid = new Asteroid(mesh, vert, scale);
-        m_asteroids->insert(asteroid);
+		// get position of the asteroid
+        	vert = Randomizer::instance()->getRandomVertex(5000);
+
+		// get scale of the asteroid
+		scale = Randomizer::instance()->getRandomNumber(1, 5);
+        	m_asteroids->insert(new Asteroid(mesh, vert, scale));
 	}
 
 }
 
-AsteroidField::~AsteroidField()
+
+void render_asteroid(Asteroid* a)
 {
-    delete m_asteroids;
+	// call render function of asteroid
+	a->render();
 }
 
 
 void AsteroidField::render()
 {
+	// render all asteroids in the listy
 	m_asteroids->for_each(render_asteroid);
 }
 
-void AsteroidField::render_asteroid(Asteroid a)
-{
-	a.render();
-}
 
 }
